6bfbac30a4e3d8f240ffa8a9fc943e2026a87fe5
Dine Mashlenko <outofdelorean@gmail.com>
2020-11-29 19:05:26 -0800

    homework 5 initial commit

---
+++ projects/async/index.js
@@ -0,0 +1,125 @@
+/*
+ Страница должна предварительно загрузить список городов из
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ и отсортировать в алфавитном порядке.
+
+ При вводе в текстовое поле, под ним должен появляться список тех городов,
+ в названии которых, хотя бы частично, есть введенное значение.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+
+ Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+ После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+
+ Разметку смотрите в файле towns.html
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+
+ *** Часть со звездочкой ***
+ Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+ При клике на кнопку, процесс загрузки повторяется заново
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+
+import { loadAndSortTowns } from './functions.js';
+import './towns.html';
+
+const homeworkContainer = document.querySelector('#app');
+
+/*
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ */
+let list = [];
+
+function loadTowns() {
+  return loadAndSortTowns();
+}
+
+/*
+ Функция должна проверять встречается ли подстрока chunk в строке full
+ Проверка должна происходить без учета регистра символов
+
+ Пример:
+   isMatching('Moscow', 'moscow') // true
+   isMatching('Moscow', 'mosc') // true
+   isMatching('Moscow', 'cow') // true
+   isMatching('Moscow', 'SCO') // true
+   isMatching('Moscow', 'Moscov') // false
+ */
+function isMatching(full, chunk) {
+  return full.toLowerCase().includes(chunk);
+}
+
+const loading = async () => {
+  try {
+    list = await loadTowns();
+    loadingBlock.classList.add('hidden');
+    filterBlock.classList.remove('hidden');
+  } catch (e) {
+    loadingFailedBlock.classList.remove('hidden');
+  }
+};
+
+/* Блок с надписью "Загрузка" */
+const loadingBlock = homeworkContainer.querySelector('#loading-block');
+/* Блок с надписью "Не удалось загрузить города" и кнопкой "Повторить" */
+const loadingFailedBlock = homeworkContainer.querySelector('#loading-failed');
+/* Кнопка "Повторить" */
+const retryButton = homeworkContainer.querySelector('#retry-button');
+/* Блок с текстовым полем и результатом поиска */
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+/* Текстовое поле для поиска по городам */
+const filterInput = homeworkContainer.querySelector('#filter-input');
+/* Блок с результатами поиска */
+const filterResult = homeworkContainer.querySelector('#filter-result');
+
+retryButton.addEventListener('click', () => {
+  loadingFailedBlock.classList.add('hidden');
+  loading();
+});
+
+filterInput.addEventListener('input', function (e) {
+  if (filterInput.value) {
+    const arr = matchList(list, filterInput.value);
+    showList(arr);
+  } else {
+    clearList();
+  }
+});
+
+const clearList = () => {
+  filterResult.innerHTML = '';
+};
+
+const matchList = (list, value) => {
+  const arr = list.map((el) => el.name);
+  return arr.filter((cur) => isMatching(cur, value));
+};
+
+const showList = (arr) => {
+  clearList();
+  arr.forEach((cur) => {
+    const li = document.createElement('div');
+    li.textContent = cur;
+    filterResult.appendChild(li);
+  });
+};
+
+loadingFailedBlock.classList.add('hidden');
+filterBlock.classList.add('hidden');
+loadingBlock.classList.remove('hidden');
+
+loading();
+
+export { loadTowns, isMatching };
